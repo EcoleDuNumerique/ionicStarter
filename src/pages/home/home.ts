c9d15efff6d0268ff07caa58c7316b5a54603651
Ribes Alexandre <ribes.alexandre@gmail.com>
2017-11-15 18:53:52 +0100

    Add Readme

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {NameComponent} from "../../components/name/name";
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  entryComponents: [
+    NameComponent
+  ]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public name: string;
 
+  constructor(public navCtrl: NavController) {
+    this.name = 'Alexandre';
   }
 
 }
